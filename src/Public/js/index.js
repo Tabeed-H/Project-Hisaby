18c490f10c7447602b7f09ca19071d5f353318f5
tabeed-h <tabeedhameed121@gmail.com>
2021-12-15 22:49:09 +0530

    Changes Made to 404 Page

--- src/Public/js/index.js
+++ src/Public/js/index.js
@@ -1,6 +1,12 @@
 console.log("Connected");
 // const loginbtn = document.querySelectorAll(".login-btn");
 const signUpBtn = document.querySelectorAll(".signup-btn");
+const logoAction = document.querySelector(".header-logo-container");
+
+logoAction.addEventListener("click", (e) => {
+  e.preventDefault();
+  window.location.assign(window.location.origin);
+});
 
 signUpBtn.forEach((btn) =>
   btn.addEventListener("click", (e) => {
