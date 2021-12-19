889038054ab59a95a89612e32193ede90f7b342a
tabeed-h <tabeedhameed121@gmail.com>
2021-12-20 00:36:40 +0530

    Added Authorization

---
+++ src/middeware/auth.controller.js
@@ -0,0 +1,21 @@
+const User = require("../User/user.Model");
+const jwt = require("jsonwebtoken");
+
+exports.doAuth = function (req, res, next) {
+  try {
+    const token = req.header("Authorization").replace("Bearer", "");
+    const decodedToken = jwt.verify(token, "sampleProject");
+
+    const user = await User.findOne({
+      _id: decodedToken,
+      "tokens.token": token,
+    });
+
+    if (!user) throw new Error();
+
+    (req.user = user), (req.token = token);
+    next();
+  } catch (err) {
+    res.status(401).send("Authentication Failed!");
+  }
+};
