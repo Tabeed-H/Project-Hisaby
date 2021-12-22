4f32cc52ce670e05c04b8c76433dc6aa88e0c514
tabeed-h <tabeedhameed121@gmail.com>
2021-12-23 01:53:21 +0530

    Added Get Route

--- src/Exp/exp.Services.js
+++ src/Exp/exp.Services.js
@@ -10,3 +10,15 @@ exports.doAdd = async (req, res) => {
     res.status(400).send(err);
   }
 };
+
+exports.doGetExp = async (req, res) => {
+  try {
+    await req.user.populate({
+      path: "exp",
+    });
+
+    res.status(200).send(req.user.exp);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+};
